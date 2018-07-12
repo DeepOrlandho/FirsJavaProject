dd0d41b9420d7a7905dec638901a163b312da126
javam2018 <javam2018@S10-96645.redes.local>
2018-07-12 11:03:12 -0600

    My first Changes

---
+++ JavaEdgarApplication0/src/javaedgarapplication0/JavaEdgarApplication0.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaedgarapplication0;
+
+/**
+ *
+ * @author javam2018
+ */
+public class JavaEdgarApplication0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Guatemala Inmortal!");
+        }
+    
+}
